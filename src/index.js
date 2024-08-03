afe20359bd6005daf5c701d79cbcc1459977fadf
Gerson Ramos <gersonlopesr@gmail.com>
2024-08-03 00:39:54 -0300

    feat: alteração código
    Alteração no código para que as páginas  html funcionem na imagem Docker.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -8,38 +9,19 @@ const nbaRoutes = require('./routes/nbaRoutes');
 app.use(cors()); 
 app.use('/nba', nbaRoutes);
 
+// Servir arquivos estáticos da pasta 'src'
+app.use(express.static(path.join(__dirname)));
+
+// Rota para a página principal
 app.get('/', (req, res) => {
-    res.send('API da NBA em Node.js');
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Rota para a página de times
+app.get('/team', (req, res) => {
+    res.sendFile(path.join(__dirname, 'team.html'));
 });
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
-
-function updateStats() {
-    const team1Name = document.getElementById("team1Input").value;
-    const team2Name = document.getElementById("team2Input").value;
-
-    const team1Stats = teamsData[team1Name];
-    const team2Stats = teamsData[team2Name];
-
-    if (team1Stats) {
-        document.getElementById("team1").querySelector("h2").textContent = team1Name;
-        document.getElementById("team1Stats").innerHTML = `
-            <li>Pontos por jogo: ${team1Stats.points}</li>
-            <li>Rebotes por jogo: ${team1Stats.rebounds}</li>
-            <li>Assistências por jogo: ${team1Stats.assists}</li>
-            <li>Porcentagem de vitória: ${team1Stats.winPercentage}%</li>
-        `;
-    }
-
-    if (team2Stats) {
-        document.getElementById("team2").querySelector("h2").textContent = team2Name;
-        document.getElementById("team2Stats").innerHTML = `
-            <li>Pontos por jogo: ${team2Stats.points}</li>
-            <li>Rebotes por jogo: ${team2Stats.rebounds}</li>
-            <li>Assistências por jogo: ${team2Stats.assists}</li>
-            <li>Porcentagem de vitória: ${team2Stats.winPercentage}%</li>
-        `;
-    }
-}
